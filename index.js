c9a5d4d743fc4046bde24327203c7118f6754272
jeff-burns <1jeffreyburns@gmail.com>
2018-05-03 16:10:12 -0600

    finished drill 3

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const data = require('./data.json')
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json())
+
+function filterById(data, id) {
+    return data.filter(student => student.ID == id)
+}
+
+app.get('/', (req, res) => {
+    res.json(data)
+})
+
+app.get('/:id', (req, res) => {
+    let result = filterById(data, req.params.id)
+    if (!result[0]) {
+        res.status(404).json({
+            error: {
+                "message": "No record found!"
+            }
+        })
+    } else {
+        res.json({data: result});
+        }
+    
+})
+
+app.listen(PORT)
