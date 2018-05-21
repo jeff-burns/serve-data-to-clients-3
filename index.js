1c9cdf0d79d7899ba1381a6080619834c40b1a66
jeff-burns <1jeffreyburns@gmail.com>
2018-05-21 11:00:29 -0600

    resubmitted

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const data = require('./data.json')
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const data = require("./data.json");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 function filterById(data, id) {
-    return data.filter(student => student.ID == id)
+  return data.filter(student => student.id == id);
 }
 
-app.get('/', (req, res) => {
-    res.json(data)
-})
+app.get("/", (req, res) => {
+  res.json({ data });
+});
 
-app.get('/:id', (req, res) => {
-    let result = filterById(data, req.params.id)
-    if (!result[0]) {
-        res.status(404).json({
-            error: {
-                "message": "No record found!"
-            }
-        })
-    } else {
-        res.json({data: result});
-        }
-    
-})
+app.get("/:id", (req, res) => {
+  let result = filterById(data, req.params.id);
+  if (!result[0]) {
+    res.status(404).json({
+      error: {
+        message: "No record found!"
+      }
+    });
+  } else {
+    res.json({ data: result });
+  }
+});
 
-app.listen(PORT)
+app.listen(PORT);
